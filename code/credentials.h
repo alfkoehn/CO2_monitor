0c98e22e44f05ad865b90864b28fc42eb6802703
Alf Köhn <alf.koehn@posteo.net>
2020-12-27 22:06:18 +0100

    Initial commit

---
+++ code/credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// enter SSID (wifi-network name) and password
+#define WIFI_SSID "ENTER_WIFI_SSID_HERE"
+#define WIFI_PASSWORD "ENTER_WIFI_PASSWORD_HERE"
+
+#endif
